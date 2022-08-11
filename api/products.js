9c7bd9633f14340b68498ae77f9fc1c79da90a32
dustinferioli <dustin.ferioli@icloud.com>
2022-08-11 14:51:12 -0400

    created products router

---
+++ api/products.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const productsRouter = express.Router();
+const { getAllProducts } = require('../db/models')
+
+productsRouter.use((req, res, next) => {
+  console.log("A request is being made to /products");
+  next();
+})
+
+productsRouter.get('/', async (req, res, next) => {
+  try {
+    const products = await getAllProducts();
+    res.send(products)
+    next();
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = productsRouter;
