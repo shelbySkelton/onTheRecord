daca3c52f44e085d54d7de454a9259cd1d52d4ed
Shelby Skelton <shelbyskelton09@gmail.com>
2022-08-17 16:04:57 -0400

    added Header.js component to fix useStates

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { testMe } from "../axios-services/users";
+import {
+    BrowserRouter as Router,
+    Link
+  } from 'react-router-dom';
+
+
+const Header = ({ isLoggedIn, setIsLoggedIn, user, setUser, isAdmin, setIsAdmin }) => {
+
+
+
+    useEffect(() => {
+        testMe()
+            .then(user => {
+                setUser(user);
+                if (!(user === {})) {
+                    setIsLoggedIn(true);
+                }       
+            })
+    }, [])
+
+
+
+    return (
+        <div className="header-container">
+        <navbar className='header-nav'>
+            <Link to='/home'
+                hidden={isLoggedIn ? false : true}
+                onClick={(evt) => {
+                    localStorage.removeItem('token');
+                    setIsLoggedIn(false);
+                    setUser({});
+                }}> Log Out
+            </Link>
+
+            <Link to="/login"
+                hidden={isLoggedIn ? true : false}
+            >Log in</Link>
+            <Link to="/register"
+                hidden={isLoggedIn ? true : false}
+
+            >Register</Link>
+            <Link
+                hidden={isLoggedIn ? false : true}
+                to="/myAccount">{user.first_name}'s Account</Link>
+            <Link to="/cart">My Cart</Link>
+
+        </navbar>
+        </div>
+    )
+}
+
+
+export default Header;
